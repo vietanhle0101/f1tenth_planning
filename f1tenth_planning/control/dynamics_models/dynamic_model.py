bf0ed6a6a7284592713d88a2d60ceb23c16dd0f0
Ahmad Amine <ahmadamine998@gmail.com>
2025-03-25 16:38:45 -0400

    Add f_casadi to dynamic model

--- f1tenth_planning/control/dynamics_models/dynamic_model.py
+++ f1tenth_planning/control/dynamics_models/dynamic_model.py
@@ -3,6 +3,7 @@ from f1tenth_planning.control.dynamics_model import Dynamics_Model
 from f1tenth_planning.control.config.dynamics_config import dynamics_config
 
 import numpy as np
+import casadi as ca
 
 class Dynamic_Model(Dynamics_Model):
     """
@@ -80,7 +81,88 @@ class Dynamic_Model(Dynamics_Model):
 
         return np.array([dx, dy, ddelta, dv, dyaw, ddyaw, dslip_angle])
     
+    def f_casadi(self, params = None):
+        if params is not None:
+            self.params = params
+
+            
+        x = ca.SX.sym('x')
+        y = ca.SX.sym('y')
+        delta = ca.SX.sym('delta')
+        v = ca.SX.sym('v')
+        yaw = ca.SX.sym('yaw')
+        yaw_rate = ca.SX.sym('yaw_rate')
+        slip_angle = ca.SX.sym('slip_angle')
+        states = ca.vertcat(
+            x,
+            y,
+            delta,
+            v,
+            yaw,
+            yaw_rate,
+            slip_angle
+        )
+        # control symbolic variables
+        a = ca.SX.sym('a')
+        delta_v = ca.SX.sym('delta_v')
+        controls = ca.vertcat(
+            delta_v,
+            a
+        )
+
+        # Extract params for more readable equations
+        mu = self.params.MU
+        m = self.params.M
+        I = self.params.I
+        lr = self.params.LR
+        lf = self.params.LF
+        C_Sf = self.params.C_SF
+        C_Sr = self.params.C_SR
+        h = self.params.H
+        g = 9.81
+
+        dyaw_slow = v * ca.cos(slip_angle) * ca.tan(delta) / (lr + lf)
+        d_beta_slow = (lr * delta_v) / ((lr + lf) * ca.cos(delta) ** 2 * (1 + (ca.tan(delta) ** 2 * lr / (lr + lf)) ** 2))
+        dyaw_rate_slow = 1 / (lr + lf) * (a * ca.cos(slip_angle) * ca.tan(delta) -
+                            v * ca.sin(slip_angle) * ca.tan(delta) * d_beta_slow  +
+                            v * ca.cos(slip_angle) * delta_v / (ca.cos(delta) ** 2))
+        
+        dyaw_fast = yaw_rate                # dyaw/dt = yaw_rate
+        dyaw_rate_fast = -mu * m / (v * I * (lr + lf)) * (
+                                        lf ** 2 * C_Sf * (g * lr - a * h) + lr ** 2 * C_Sr * (g * lf + a * h)) * yaw_rate \
+                            + mu * m / (I * (lr + lf)) * (lr * C_Sr * (g * lf + a * h) - lf * C_Sf * (g * lr - a * h)) * slip_angle \
+                            + mu * m / (I * (lr + lf)) * lf * C_Sf * (g * lr - a * h) * delta # dyaw_rate/dt = RHS
+        d_beta_fast = (mu / (v ** 2 * (lr + lf)) * (C_Sr * (g * lf + a * h) * lr - C_Sf * (g * lr - a * h) * lf) - 1) * yaw_rate \
+                            - mu / (v * (lr + lf)) * (C_Sr * (g * lf + a * h) + C_Sf * (g * lr - a * h)) * slip_angle \
+                            + mu / (v * (lr + lf)) * (C_Sf * (g * lr - a * h)) * delta    # dbeta/dt = RHS
+        
+        RHS_LOW_SPEED = ca.vertcat(
+                            v * ca.cos(yaw + slip_angle),  # dx/dt = v * cos(yaw + slip_angle)
+                            v * ca.sin(yaw + slip_angle),  # dy/dt = v * sin(yaw + slip_angle)
+                            delta_v,                 # d(delta)/dt = delta_v
+                            a,                       # dv/dt = a
+                            dyaw_slow,  # dyaw/dt = yaw_rate
+                            dyaw_rate_slow,                  # dyaw_rate/dt = RHS
+                            d_beta_slow                   # dbeta/dt = d_beta
+                        ) # dx/dt = f(x,u)
+    
+        RHS_DYN = ca.vertcat(
+                            v * ca.cos(yaw + slip_angle),  # dx/dt = v * cos(yaw + slip_angle)
+                            v * ca.sin(yaw + slip_angle),  # dy/dt = v * sin(yaw + slip_angle)
+                            delta_v,                 # d(delta)/dt = delta_v
+                            a,                       # dv/dt = a
+                            dyaw_fast,  # dyaw/dt = yaw_rate
+                            dyaw_rate_fast,                  # dyaw_rate/dt = RHS
+                            d_beta_fast                   # dbeta/dt = d_beta
+                        ) # dx/dt = f(x,u)
+
+        RHS = ca.if_else(v >= 0.1, RHS, RHS_LOW_SPEED)
+
+        # maps controls from [va, vb, vc, vd].T to [vx, vy, omega].T
+        f = ca.Function('f', [states, controls], [RHS])
+        
     def linearize_around_state(self, state: np.ndarray, control: np.ndarray, params: dynamics_config = None) -> tuple[np.ndarray, np.ndarray]:
+        raise NotImplementedError("Linearization not implemented for dynamic model yet.")
         x, y, delta, v, yaw, yaw_rate, slip_angle = state
         delta_v, a = control
 
