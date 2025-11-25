a4f6b0efe259e6368e0a376bc504aa398556ea15
Ahmad Amine <ahmadamine998@gmail.com>
2025-11-25 14:36:30 -0500

    Add model config for neural network models

--- f1tenth_planning/control/config/__init__.py
+++ f1tenth_planning/control/config/__init__.py
@@ -1,2 +1,3 @@
 from .controller_config import *
 from .dynamics_config import *
+from .model_config import *
---
+++ f1tenth_planning/control/config/model_config.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class model_config:
+    """
+    Neural model/value-function hyperparameters (kept simple; extend as needed).
+    """
+
+    hidden_dim: int = 256
+    hidden_layers: int = 4
+    learning_rate: float = 5e-4
+    model_type: str = "nf"  # options: nf, bnn, mlp
+    max_epoch: int = 200
+    ensemble_size: int = 1
